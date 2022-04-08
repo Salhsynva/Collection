670e7c76544135ceaa13ae21132bb5d11e17ef29
Salhsynva <100703384+Salhsynva@users.noreply.github.com>
2022-04-08 09:54:45 +0400

    Add files via upload

---
+++ Collection/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>();
+            string answer;
+            do
+            {
+                Console.WriteLine("-----MENU-----");
+                Console.WriteLine("1.telebe elave et\n2.telebeye imtahan elave et \n3.telebenin 1 imtahan balina bax \n4.telebenin butun imtahan ballarini goster \n5.telebenin imtahan ortalamasini goster \n6.telebeden imtahan sil \n0.Proqrami bitir");
+                answer = Console.ReadLine();
+                switch (answer)
+                {
+                    case "1":
+                        Console.WriteLine("telebenin adini daxil edin: ");
+                        string fullname = Console.ReadLine();
+                        Student student = new Student();
+                        student.Fullname = fullname;
+                        students.Add(student);
+                        break;
+                    case "2":
+                        int no2 = GetStudentNo(students);
+                        string examName = GetExamName();
+                        int point = GetStudentPoint();
+                        students[no2].AddExam(examName, point);
+                        break;
+
+                    case "3":
+                        int no3 = GetStudentNo(students);
+                        string examName3 = GetExamName();
+                        Console.WriteLine(students[no3].GetExamResult(examName3));
+                        break;
+                    case "4":
+                        int no4 = GetStudentNo(students);
+                        students[no4].GetInfo();
+                        break;
+                    case "5":
+                        int no5 = GetStudentNo(students);
+                        Console.WriteLine(students[no5].GetExamAvg()); 
+                        break;
+                    case "6":
+                        int no6 = GetStudentNo(students);
+                        string examName6 = GetExamName();
+                        students[no6].Exams.Remove(examName6);
+                        break;
+                    case "0":
+                        break;
+                    default:
+                        Console.WriteLine("menuda bel secim yoxdur!");
+                        break;
+                }
+            } while (answer != "0");
+        }
+        static int GetStudentNo(List<Student> students)
+        {
+            Console.WriteLine("telebenin nomresini daxil edin: ");
+            string nostr = Console.ReadLine();
+            int no = 0;
+            bool check = false;
+            while (!check)
+            {
+                while (!int.TryParse(nostr, out no))
+                {
+                    Console.WriteLine("eded daxil edin! ");
+                    nostr = Console.ReadLine();
+                }
+                foreach (var item in students)
+                {
+                    if (item.No == no)
+                        check = true;
+                }
+                if (!check)
+                {
+                    Console.WriteLine("duzgun deyer daxil edin!");
+                    nostr = Console.ReadLine();
+                }
+            }
+            return no;
+        }
+        static string GetExamName()
+        {
+            Console.WriteLine("imtahan adi qeyd edin: ");
+            string examName = Console.ReadLine();
+            return examName;
+        }
+        static int GetStudentPoint()
+        {
+            Console.WriteLine("imtahan balini daxil edin: ");
+            string pointStr = Console.ReadLine();
+            int point = 0;
+            bool check = false;
+            while (!check)
+            {
+                while (!int.TryParse(pointStr, out point))
+                {
+                    Console.WriteLine("eded daxil edin!");
+                    pointStr = Console.ReadLine();
+                }
+                while (int.TryParse(pointStr, out point))
+                {
+                    if (point >= 0 && point <= 100)
+                    {
+                        check = true;
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("0 ve 100 arasi eded daxil edin!");
+                        pointStr = Console.ReadLine();
+                    }
+                }
+            }
+            return point;
+        }
+    }
+}
---
+++ Collection/Student.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Collection
+{
+    public class Student
+    {
+        public Student()
+        {
+            _totalCount++;
+            No = _totalCount;
+        }
+        static int _totalCount;
+        public int No { get;  }
+        public string Fullname { get; set; }
+        public Dictionary<string, int> Exams = new Dictionary<string, int>();
+        public void AddExam(string examName, int point)
+        {
+            foreach (var exam in Exams)
+            {
+                if (exam.Key == examName)
+                    return;
+            }
+            Exams.Add(examName, point);
+        }
+        public int GetExamResult(string examName)
+        {
+            foreach (var item in Exams)
+            {
+                if (item.Key == examName)
+                    return item.Value;
+            }
+            return -1;
+        }
+        public double GetExamAvg()
+        {
+            int sum = 0;
+            int count = 0;
+            foreach (var item in Exams)
+            {
+                sum += item.Value;
+                count++;
+            }
+            if (count > 0)
+                return sum / count;
+            return 0;
+        }
+        public void GetInfo()
+        {
+            foreach (var item in Exams)
+            {
+                Console.WriteLine(item.Key+ " : " + item.Value);
+            }
+        }
+
+
+    }
+}
